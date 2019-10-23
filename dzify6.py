dda43459a6dac559beb57d5c7bd4d03705383266
arzabolotskaya <55873137+arzabolotskaya@users.noreply.github.com>
2019-10-23 15:05:09 +0300

    Add files via upload

---
+++ dzify6.py
@@ -0,0 +1,11 @@
+print('last task, mate, A v stepeni B will be C')
+
+a = int(input('Enter A number: '))
+b = int(input('Enter B number: '))
+c = int(input('Enter C number: '))
+
+x = a ** b
+if c == x:
+    print('You know stepeni, congrats')
+else:
+    print('Too steeeped to know stepeni? Try again!')
